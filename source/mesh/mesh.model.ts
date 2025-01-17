54ab4ee0cdaa4e9ce588182925f57e44a1d313bd
Alexey Skurlatov <Scarlatum@yandex.ru>
2025-01-17 14:53:29 +0800

    Mesh: Update of a mesh layout

--- source/mesh/mesh.model.ts
+++ source/mesh/mesh.model.ts
@@ -18,10 +18,10 @@ export const enum VERTEX_LAYOUT {
 
 export interface IMesh {
   id: symbol,
-  uv: Nullable<Float32Array>,
-  normals: Nullable<Float32Array>,
+  uv: Nullable<WeakRef<Float32Array>>,
+  normals: Nullable<WeakRef<Float32Array>>,
   material: Nullable<ProceduredMaterial>,
-  vertexes: Float32Array,
+  vertexes: WeakRef<Float32Array>,
   texture: GPUTexture;
 }
 
@@ -33,42 +33,43 @@ export interface ShadowPoperty {
 export class Mesh extends Drawable implements IMesh {
 
   static TRANSFORM_BUFFER_STRIDE = 4 * 4 * Float32Array.BYTES_PER_ELEMENT;
-  static VERTEX_SIZE = 11;
+  static VERTEX_SIZE = 9;
 
   public override instances = 1; 
-
+  public override instanceParamBuffer: GPUBuffer;
   public id = Symbol("mesh");
   public model = mat4.create();
   public transformationIndex: number = 0;
-  public uv: Nullable<Float32Array> = null;
-  public normals: Nullable<Float32Array> = null;
+  public uv: Nullable<WeakRef<Float32Array>> = null;
   public material: Nullable<ProceduredMaterial> = null;
   public texture: GPUTexture;
   override shadowCast: boolean = false;
   override shadowRecieve: boolean = false;
 
-  public vertexes: Float32Array = new Float32Array();
-  public vbo: Float32Array = new Float32Array();
+  public vertexes: WeakRef<Float32Array>;
+  public normals: Nullable<WeakRef<Float32Array>> = null;
 
   vertexBuffer;
   tranformationBuffer;
   vertexCount;
 
   constructor(
-    payload: IMesh,
+    payload: DereferencedObjectValues<IMesh>,
     shadowprop: ShadowPoperty,
   ) {
 
     super();
 
+    this.id           = payload.id
+    this.vertexes     = new WeakRef(payload.vertexes);
+    this.normals      = new WeakRef(payload.normals);
+    this.uv           = new WeakRef(payload.uv);
     this.texture      = payload.texture;
-    this.vertexes     = payload.vertexes;
-    this.normals      = payload.normals;
     this.vertexCount  = payload.vertexes.length / 3;
+    this.material     = payload.material;
 
-    Object.assign(this, payload);
-
-    this.vbo = new Float32Array(this.vertexCount * Mesh.VERTEX_SIZE);
+    // @ts-expect-error
+    globalThis.randomBuffer = this.vertexes;
 
     // vertex buffer set
     this.vertexBuffer = device.createBuffer({
@@ -84,12 +85,25 @@ export class Mesh extends Drawable implements IMesh {
         | GPUBufferUsage.STORAGE,
     });
 
-    this.shadowCast = shadowprop.cast;
-    this.shadowRecieve = shadowprop.recieve;
+    this.instanceParamBuffer = device.createBuffer({
+      size: Uint32Array.BYTES_PER_ELEMENT * 3,
+      usage: GPUBufferUsage.UNIFORM,
+      mappedAtCreation: true,
+    });
 
-    Mesh.constructVertexData(this, this.vbo, this.vertexCount);
+    const vbo = new Float32Array(this.vertexCount * Mesh.VERTEX_SIZE);
 
-    device.queue.writeBuffer(this.vertexBuffer!, 0, this.vbo);
+    Mesh.constructVertexData(this, vbo, this.vertexCount);
+
+    device.queue.writeBuffer(this.vertexBuffer!, 0, vbo);
+
+    new Uint32Array(this.instanceParamBuffer.getMappedRange()).set([
+      payload.material?.id || 0,
+      Number(this.shadowCast = shadowprop.cast),
+      Number(this.shadowRecieve = shadowprop.recieve),
+    ]);
+
+    this.instanceParamBuffer.unmap();
 
   }
 
@@ -114,30 +128,24 @@ export class Mesh extends Drawable implements IMesh {
     offset: number = 0,
   ) {
 
-    let min = vec3.create();
-    let max = vec3.create();
+    const vertexes = mesh.vertexes.deref();
+    const normals = mesh.normals?.deref();
+    const uv = mesh.uv?.deref();
+
+    if ( !vertexes ) throw Error();
 
     for (let v = 0; v < vertexCount; v++) {
-
-      const pos = [
-        mesh.vertexes[ v * 3 + 0 ], 
-        mesh.vertexes[ v * 3 + 1 ], 
-        mesh.vertexes[ v * 3 + 2 ]
-      ];
-
-      min.forEach((x,i) => min[i] = Math.min(x, pos[i]));
-      max.forEach((x,i) => max[i] = Math.max(x, pos[i]));
       
       out.set([
         mesh.transformationIndex,
-        mesh.material?.id || 0.0,
-        ...pos,
-        mesh.normals?.[ v * 3 + 0 ] || 0.0,
-        mesh.normals?.[ v * 3 + 1 ] || 0.0,
-        mesh.normals?.[ v * 3 + 2 ] || 0.0,
-        mesh.uv?.[ v * 2 + 0 ] || 0.0,
-        mesh.uv?.[ v * 2 + 1 ] || 0.0,
-        mesh.shadowRecieve ? 1.0 : 0.0,
+        vertexes[ v * 3 + 0 ], 
+        vertexes[ v * 3 + 1 ], 
+        vertexes[ v * 3 + 2 ],
+        normals?.[ v * 3 + 0 ] || 0.0,
+        normals?.[ v * 3 + 1 ] || 0.0,
+        normals?.[ v * 3 + 2 ] || 0.0,
+        uv?.[ v * 2 + 0 ] || 0.0,
+        uv?.[ v * 2 + 1 ] || 0.0,
       ], offset);
 
       offset += Mesh.VERTEX_SIZE;
@@ -152,36 +160,27 @@ export class Mesh extends Drawable implements IMesh {
     return {
       arrayStride: Float32Array.BYTES_PER_ELEMENT * Mesh.VERTEX_SIZE,
       attributes: [
-        { // ID
+        // ? Оставляю это лишь для того, что в последствии буду батчить ститические и динамические меши вместе
+        { // Transforamtion ID
           format: "float32",
           offset: Float32Array.BYTES_PER_ELEMENT * 0,
           shaderLocation: 0
         },
-        { // Material ID
-          format: "float32",
-          offset: Float32Array.BYTES_PER_ELEMENT * 1,
-          shaderLocation: 1
-        },
         { // Vertex Data
           format: "float32x3",
-          offset: Float32Array.BYTES_PER_ELEMENT * 2,
-          shaderLocation: 2,
+          offset: Float32Array.BYTES_PER_ELEMENT * 1,
+          shaderLocation: 1,
         },
         { // Normals Data
           format: "float32x3",
-          offset: Float32Array.BYTES_PER_ELEMENT * 5,
-          shaderLocation: 3,
+          offset: Float32Array.BYTES_PER_ELEMENT * 4,
+          shaderLocation: 2,
         },
         { // UV data
           format: "float32x2",
-          offset: Float32Array.BYTES_PER_ELEMENT * 8,
-          shaderLocation: 4,
+          offset: Float32Array.BYTES_PER_ELEMENT * 7,
+          shaderLocation: 3,
         },
-        { // Shadow Reciever
-          format: "float32",
-          offset: Float32Array.BYTES_PER_ELEMENT * 10,
-          shaderLocation: 5,
-        }
       ]
     };
   }
@@ -193,7 +192,7 @@ export class InstancesMesh extends Mesh {
   public models: Array<mat4>;
 
   constructor(
-    data: IMesh,
+    data: DereferencedObjectValues<IMesh>,
     shadowprop: ShadowPoperty,
     public override instances: number,
   ) {
