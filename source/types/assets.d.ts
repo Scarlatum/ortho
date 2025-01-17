5d5511765b62f2be8b1e64f950af42a78896fe48
Alexey Skurlatov <Scarlatum@yandex.ru>
2025-01-17 14:45:25 +0800

    Misc: Move up index.d.ts. Added utility types for objects with WeakRef values. Shared way of declaration debug env with Vite

--- source/types/assets.d.ts
+++ source/types/assets.d.ts
@@ -1,12 +1,10 @@
-import { Ref } from "vue";
-
 declare module '*.wgsl?raw' {
   const src: string;
   export default src;
 }
 namespace globalThis {
-  var DEBUG: { value: boolean; };
-  var device: GPUDevice;
-  var adapter: GPUAdapter;
-  var context: GPUCanvasContext;
+  var device  : GPUDevice;
+  var adapter : GPUAdapter;
+  var context : GPUCanvasContext;
+  var auctx   : AudioContext;
 }
