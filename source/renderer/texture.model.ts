aefdfda6029780a2857a2e4205ca63446128ccee
Alexey Skurlatov <Scarlatum@yandex.ru>
2025-03-01 04:29:12 +0800

    Renderer: Update `Texture` class to able recieve raw RGBA data as input

--- source/renderer/texture.model.ts
+++ source/renderer/texture.model.ts
@@ -1,5 +1,3 @@
-declare const testBit: Blob; 
-
 export class Texture {
 
   static mipsQuantity = 4;
@@ -16,13 +14,57 @@ export class Texture {
     return this.texture.height;
   }
 
+  private static createTextureTemplate(width: number, height: number) {
+    return device.createTexture({
+      format: "rgba8unorm",
+      usage: GPUTextureUsage.TEXTURE_BINDING
+        | GPUTextureUsage.COPY_DST
+        | GPUTextureUsage.COPY_SRC
+        | GPUTextureUsage.RENDER_ATTACHMENT,
+      size: {
+        width: width,
+        height: height,
+      },
+      dimension: "2d",
+      mipLevelCount: Texture.mipsQuantity + 1,
+    });
+  }
+
+  public static fromRaw(
+    raw: ArrayBuffer, 
+    mips: Array<ArrayBuffer> = [],
+    size: { width: number, height: number } = { height: 0, width: 0 }
+  ) {
+
+    const texture = Texture.createTextureTemplate(size.width, size.height);
+
+    queueMicrotask(() => {
+
+      const row = Uint8Array.BYTES_PER_ELEMENT * 4 * size.width;
+
+      device.queue.writeTexture({ texture }, raw, {
+        bytesPerRow: row,
+      }, size);
+
+      for ( let i = 0; i < mips.length; i++ ) {
+        device.queue.writeTexture({ texture }, mips[i], {
+          bytesPerRow: row >> (1 + i)
+        }, size);
+      }
+
+    });
+
+    return texture;
+
+  }
+
   public static async fromBuffer(buffer: ArrayBuffer, mips: Array<ArrayBuffer> = []): Promise<Result<Texture>> {
 
     const data = new Blob([ buffer ]);
 
     let bitmap = await createImageBitmap(data);
 
-    if ( bitmap.width < 256 || bitmap.height < 256 ) return Error("Texture should be larger than 256x256 px")
+    if ( import.meta.env.DEV && bitmap.width < 256 || bitmap.height < 256 ) return Error("Texture should be larger than 256x256 px")
 
     return Texture.create(
       bitmap, 
@@ -42,19 +84,7 @@ export class Texture {
 
     const bitmaps = new Set<ImageBitmap>();
 
-    const texture = device.createTexture({
-      format: "rgba8unorm",
-      usage: GPUTextureUsage.TEXTURE_BINDING
-        | GPUTextureUsage.COPY_DST
-        | GPUTextureUsage.COPY_SRC
-        | GPUTextureUsage.RENDER_ATTACHMENT,
-      size: {
-        width: width,
-        height: height,
-      },
-      dimension: "2d",
-      mipLevelCount: Texture.mipsQuantity + 1,
-    });
+    const texture = Texture.createTextureTemplate(width, height);
 
     const send = (x: ImageBitmap, level: number, height: number, width: number) => {
 
